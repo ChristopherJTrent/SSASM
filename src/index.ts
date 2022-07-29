d511bc5588e439ceb46c5437f982d5277ef4f412
ChristopherJTrent <ChristopherJTrent@outlook.com>
2022-07-29 16:26:52 -0700

    added another example script. it is currently unfinished

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { compile } from "./adapter/compiler"
 import { readFileSync } from 'fs'
 import { runtime } from "./adapter/runtime"
 
-const PROGRAM = readFileSync(__dirname + '/testcode.ssasm', 'utf-8')
+const PROGRAM = readFileSync(__dirname + '/takattack.ssasm', 'utf-8')
 let program = compile(PROGRAM.split('\n'))
 //program.toString()
 //console.log(program.toString())
